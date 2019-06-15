cedf4b61b19c713fdbb132252b0de6d11ba160ce
Paul Valente <paul.val1@hotmail.com>
2019-06-15 18:28:44 +0200

    added chef and 3 stars selection

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants  do
+    collection do
+      get "top", to: "restaurants#top"
+    end
+    member do
+      get "chef", to: "restaurants#chef"
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        5,
+    chef:         'Jammy Oliver'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4,
+    chef:         "Gordon Ramrod"
+  },
+    {
+    name:         'Ipecac au Bristol',
+    address:      '12 High Street, Temple-Meads',
+    description:  'Unique fusion cuisine mixing Asian and Inuit food styles.',
+    stars:        1,
+    chef:         "Fanny Craddock"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
